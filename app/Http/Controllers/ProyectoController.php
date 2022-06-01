2631d06d62ac8debd253c57f1d30fc9e5f7738b0
Alberto Ballesteros <a.ballesteros@2hingenieria.com>
2022-06-01 08:47:38 +0200

    GestionInterna

--- app/Http/Controllers/ProyectoController.php
+++ app/Http/Controllers/ProyectoController.php
@@ -25,7 +25,7 @@ class ProyectoController extends Controller
     {
 
         $rolConPoderes = self::ROLCONPODERES;
-        $proyectos = $cliente->proyectos()->paginate(10);
+        $proyectos = $cliente->proyectos()->get();
 
         return view('proyectos.index', compact('cliente', 'proyectos', 'rolConPoderes'));
     }
